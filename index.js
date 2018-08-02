fd6f68f1bd2652036e14025190a9caa390d5b0d2
Pan Cheng <pchengeducation@hotmail.com>
2018-08-02 21:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ function scuberGreetingForFeet(ride)
   if (ride <= 400){
     return "This one is on me!"
     }
-  else if (ride > 2000){
+  else if (ride > 400 && ride <= 2500){
     return "I will gladly take your thirty bucks."
-    if (ride > 2500)
+  }
+    else if (ride > 2500)
     {
       return "No can do."
     }
-    }
-
-
-}
+  }
