52b62245661bfa86e0c15586d2d324666ea0f16a
Pan Cheng <pchengeducation@hotmail.com>
2018-08-02 21:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function scuberGreetingForFeet(ride)
     if (tip === "generous"){
       return "Thank you so much."
     }
-    else if (tip !=== "generous"){
+    else if (tip !== "generous"){
       return "Thank you."
     }
   }
